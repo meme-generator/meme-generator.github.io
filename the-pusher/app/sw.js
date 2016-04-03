43dd10d0c7cd466d9a41c7faadc5a0225a2a89dc
Chris Saunders <slapheadted@gmail.com>
2016-04-03 01:07:37 -0700

    Added first draft of The-Pusher #new-app

---
+++ the-pusher/app/sw.js
@@ -0,0 +1,63 @@
+/*
+*
+*  Push Notifications codelab
+*  Copyright 2015 Google Inc. All rights reserved.
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*      https://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License
+*
+*/
+
+// Version 0.1
+
+'use strict';
+
+console.log('Started', self);
+
+self.addEventListener('install', function(event) {
+  self.skipWaiting();
+  console.log('Installed', event);
+});
+
+self.addEventListener('activate', function(event) {
+  console.log('Activated', event);
+});
+
+self.addEventListener('push', function(event) {
+  event.waitUntil(
+    self.registration.pushManager.getSubscription().then(function(subscription) {
+      fetch('./payload.json', {
+        method: 'post',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(subscription)
+      })
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        self.registration.showNotification(data.title, {
+          body: data.body,
+          icon: 'favicon-196x196.png'
+        });
+      })
+      .catch(function(err) {
+        console.log('err');
+        console.log(err);
+      });
+    })
+  );
+});
+
+// TODO
